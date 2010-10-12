da7a0e9f4beefb88e1b5027e5fbff7daf21e6604
Fernando <fernandoa.reyes@gmail.com>
2010-10-12 00:30:07 -0300

    Listo con las paginas estaticas

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -11,4 +11,9 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
 end
