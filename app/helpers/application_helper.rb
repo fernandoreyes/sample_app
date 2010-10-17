ec1a23597250e17272799fb7e92238a8f6c544aa
Fernando <fernandoa.reyes@gmail.com>
2010-10-17 04:09:52 -0300

    Layout y rutas terminadas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+   
+   # Devuelve un title cuando no esta definido para la pagina
+   def title
+      base_title ="Aplicacion Ruby on Rails"
+      if @title.nil?
+         base_title
+      else
+         "#{base_title} | #{@title}"
+      end
+   end   
 end
